4043685838b6b06125bc754260ad568940dea158
Silverslide <roelstorms@hotmail.com>
2013-03-07 13:44:28 +0100

    deleted:    DataHandler.cpp~ 	modified:   HTTP/http.cpp 	modified:   HTTP/http.h 	modified: HTTP/http.o 	modified:   HTTP/http_test.cpp 	modified:   HTTP/http_test.out 	deleted:    HTTP/ipsum_destination~ 	modified:   HTTP/log.txt 	deleted:    HTTP/log.txt~ 	renamed:    HTTP/input~ -> HTTP/loginReply 	deleted:    SerialCom/inputhandler.cpp~ 	deleted:    SerialCom/inputhandler.h~ 	deleted:    SerialCom/main.cpp~ 	deleted:    SerialCom/makefile~ 	modified:   XML/XML.cpp 	deleted:    XML/XML.cpp~ 	deleted:    XML/XML_test.cpp~

--- HTTP/http.h
+++ HTTP/http.h
@@ -40,19 +40,23 @@ class Http
 	  	CURL *curl;	
 		std::string urlBase;
 		std::string personalKey;
+		std::string token;
 	public: 
 		Http(std::string urlBase);
 		~Http();
 		std::string calculateDestination(int userID, int installationID, int sensorGroupID, int sensorID);
 		size_t read_data( void *ptr, size_t size, size_t nmemb, void *userdata);
-		static size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp);
+		size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp);
 		void sendGet(std::string urlAddition);
-		void sendPost(std::string urlAddition, std::string data);
+		void sendPost(std::string urlAddition, std::string data, size_t (Http::*callback) (void *, size_t, size_t, void *));
+		size_t loginReply(void *buffer, size_t size, size_t nmemb, void *userp);
 		std::string generateCode(std::string url);
 		std::string getCurrentTimestamp();
 		void uploadData(float data);
 		std::string toBase64(std::string input);
+		bool login();
 		std::string getEntity(std::string destinationBase64);	
+		void setToken(std::string aToken);
 };	
 
 #endif
